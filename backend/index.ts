adf6a8388bfe8e893b573ab5b737bbe9461b1378
Dxqeee <thibault62118@gmail.com>
2025-03-08 10:57:20 +0100

    Add typescript support

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import router from "./src/test.ts";
 import "dotenv/config";
 
 const port = process.env.PORT ?? 5000;
 const app = express();
+
+app.use(router);
 
 app.listen(port, () => {
 	console.log(`Server is listening at port ${port}`);
